39c1b6db3f770da04217eb8dcc2c011eb0be75b4
luuiis00057 <83648183+luuiis00057@users.noreply.github.com>
2025-06-02 20:19:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", function () {
+    menu.classList.toggle("active");
+  });
+
+  const formulario = document.getElementById('formulario-contato');
+
+  formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value.trim();
+    const mensagem = document.getElementById('mensagem').value.trim();
+
+    const texto = `Olá, me chamo *${nome}*.
+
+
+Mensagem:
+${mensagem}`;
+
+    const textoCodificado = encodeURIComponent(texto);
+
+    const numeroWhatsApp = '556282758178';
+    const url = `https://wa.me/556282758178?text=${textoCodificado}`;
+
+    window.open(url, '_blank');
+  });
+});
